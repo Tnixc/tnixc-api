cced7824fd09806b367418d2634f37d2ba32e2a6
Juan Antonio <juananthonio23@gmail.com>
2024-11-07 12:43:31 +0700

    fix: error

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,7 @@
-/**
- * This file is the entrypoint for all Vercel Functions.
- */
-
-// import '../src/env.ts';
-
-import { app } from '../src/app';
+// eslint-disable-next-line ts/ban-ts-comment
+// @ts-expect-error
+// eslint-disable-next-line antfu/no-import-dist
+import { app } from '../dist/src/app';
 
 // export const config = { runtime: 'edge' };
 export const runtime = 'edge';
