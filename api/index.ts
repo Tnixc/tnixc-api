909f426a32be2adf89d366541d56a8bc805f2819
Juan Antonio <juananthonio23@gmail.com>
2024-11-07 12:46:23 +0700

    fix: error

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line ts/ban-ts-comment
 // @ts-expect-error
 // eslint-disable-next-line antfu/no-import-dist
-import { app } from '../dist/src/app';
+import { app } from '../dist/src';
 
 // export const config = { runtime: 'edge' };
 export const runtime = 'edge';
