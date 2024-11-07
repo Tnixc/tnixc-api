d5abfed4ef010a617ed829e90aa683b7d9fe8978
Juan Antonio <juananthonio23@gmail.com>
2024-11-07 11:34:32 +0700

    fix: error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import app from './app';
+import { Elysia } from 'elysia';
+import dotenv from 'dotenv';
+import env from './env';
+import appRoute from './routes';
+
+dotenv.config();
+
+const app = new Elysia().use(appRoute);
+
+const port = env.PORT || 3000;
+
+app.listen(port);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
-
-Bun.serve({
-  fetch: app.fetch,
-  port: app.server?.port,
-});
