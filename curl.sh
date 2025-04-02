89b671a7c99f2e56b7471bcf74df508ba93b0db3
Tnixc <tnixxc@gmail.com>
2025-04-02 14:21:39 -0400

    rs

--- curl.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST https://rust-vercel-gamma.vercel.app/api/handler -H "Content-Type: application/json" -d '{
-  "key1": "value1",
-  "key2": "value2",
-  "key_array": ["item1", "item2", "item3"]
-}'
