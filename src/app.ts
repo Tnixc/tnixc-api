195c61d19bfb3526f84bb6ed433a810b1cf7d624
Tnixc <tnixxc@gmail.com>
2025-04-02 13:52:26 -0400

    new

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import appRoute from '@/routes';
-import { createElysia } from '@/utils/elysia';
-import { okResponse } from '@/utils/response';
-
-export const app = createElysia()
-  .use(appRoute)
-  .get('/health', () => okResponse({ status: 'ok' }));
-
-export type App = typeof app;
--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import Elysia from 'elysia';
-
-const appRoute = new Elysia();
-
-appRoute.get('/', () => "hello elysia~");
-
-export default appRoute;
