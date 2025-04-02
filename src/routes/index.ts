9e96c0bb890e1e6932dc97ed7de777b89053b946
Tnixc <tnixxc@gmail.com>
2025-04-02 13:47:44 -0400

    mroe

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,7 @@
-import { getAppDetail } from '../controllers/homeController';
-import { getAllUser } from '../controllers/userController';
 import Elysia from 'elysia';
 
 const appRoute = new Elysia();
 
-appRoute.get('/', getAppDetail);
-appRoute.get('/users', getAllUser);
+appRoute.get('/', () => "hello elysia~");
 
 export default appRoute;
